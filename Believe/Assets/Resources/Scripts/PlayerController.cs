1a8d6afeaa4b39b0b65d2175933878be4b52b2b2
Calvin0630 <cg7057705@gmail.com>
2019-04-26 19:39:45 -0400

    character controller works.

---
+++ Believe/Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float playerSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 deltaDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * playerSpeed;
+        gameObject.transform.Translate(deltaDir);
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.y = gameObject.transform.position.y;
+        Vector3 lookDir = mouseWorldPos - gameObject.transform.position;
+        Debug.Log("forward: " + gameObject.transform.forward);
+        Debug.Log("lookDir: " + lookDir);
+        if (lookDir.magnitude>0.5) gameObject.transform.forward = lookDir.normalized;
+        //gameObject.transform.LookAt(mouseWorldPos);
+    }
+}
