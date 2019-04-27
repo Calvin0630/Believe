9b1c43cf8ad56e80eab9161565c94b1f01e812f9
Zedecous <swatenya@gmail.com>
2019-04-27 12:10:01 -0400

    Added sun and new moon to SampleScene

--- Believe/Assets/Resources/Scripts/DayNightCycle.cs
+++ Believe/Assets/Resources/Scripts/DayNightCycle.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour {
-    public float speed;
+    private float speed;
 	// Use this for initialization
 	void Start () {
-        
+        speed = 3f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.forward, speed * Time.deltaTime);
         transform.LookAt(Vector3.zero);
 	}
 }
