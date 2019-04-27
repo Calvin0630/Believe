06fe259e9507df399977f4aaf009183a041faa7b
Zedecous <swatenya@gmail.com>
2019-04-26 22:28:15 -0400

    Finished day night cycle sun moon rotation

--- Believe/Assets/Resources/Scripts/DayNightCycle.cs
+++ Believe/Assets/Resources/Scripts/DayNightCycle.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour {
-
+    public float speed;
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
 	}
 }
