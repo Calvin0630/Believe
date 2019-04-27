3911e55cba18cfdadb61a11e14343d74529c0386
Zedecous <swatenya@gmail.com>
2019-04-27 18:50:48 -0400

    Added a clock to show time of day

--- Believe/Assets/Resources/Scripts/DayNightCycle.cs
+++ Believe/Assets/Resources/Scripts/DayNightCycle.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DayNightCycle : MonoBehaviour {
     private float speed;
+    public Text clock;
+    public int currentHour;
+    private float time;
 	// Use this for initialization
 	void Start () {
-        speed = 0.8f;
+        speed = 3f;
+        time = 0;
+        currentHour = 12;
+        clock.text = currentHour + ": 00";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(Vector3.zero, Vector3.forward, speed * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.left, speed * Time.deltaTime);
         transform.LookAt(Vector3.zero);
-	}
+        time += Time.deltaTime;
+        if (time >= 5)
+        {
+            time = 0;
+            currentHour++;
+            if (currentHour == 24)
+                currentHour = 0;
+        }
+        clock.text = currentHour + ": 00";
+    }
 }
